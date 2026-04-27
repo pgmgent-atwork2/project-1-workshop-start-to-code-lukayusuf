6791b9bfa4dd8a65c03189df9039e60a3e7735b5
pgm-lukagrav <lukagrav@student.arteveldehs.be>
2026-04-27 17:06:49 +0200

    Puzzle demo: add function to draw the playing field

---
+++ js/puzzle.js
@@ -0,0 +1,22 @@
+const grid = document.getElementById("puzzle-grid");
+
+function drawBoard() {
+  grid.innerHTML = "";
+
+  tiles.forEach((tileValue, index) => {
+    const div = document.createElement("div");
+    div.classList.add("tile");
+
+    if (tileValue === null) {
+      div.classList.add("emptyTile");
+    } else {
+      const x = (tileValue % 3) * -100;
+      const y = Math.floor(tileValue / 3) * -100;
+      div.style.backgroundPosition = `${x}px ${y}px`;
+      div.onclick = () => moveTile(index);
+    }
+    grid.appendChild(div);
+  });
+}
+
+drawBoard();
